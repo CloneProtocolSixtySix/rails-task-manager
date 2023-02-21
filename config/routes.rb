d1de125a1ba8c70014949391518b02296493a521
CloneProtocolSixtySix@users.noreply.github.com <yannick.bruderlein@icloud.com>
2023-02-21 14:07:34 -0500

    rtm

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb "/path", to: "controller#action", as: "prefix"
+
+  get '/tasks', to: 'tasks#index'
 end
